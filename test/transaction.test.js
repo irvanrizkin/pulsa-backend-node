827010fdd9ee71314ba7d631501c54ca00555ce2
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2021-02-26 16:55:24 +0700

    :white_check_mark: improve create transaction testing

--- test/transaction.test.js
+++ test/transaction.test.js
@@ -45,4 +45,23 @@ describe('/transaction endpoint', () => {
       });
     done();
   });
+
+  it('should have data property with same data sent before', (done) => {
+    ChaiReq
+      .post('/transaction/add')
+      .set('Content-Type', 'application/json')
+      .send({
+        phone,
+        name,
+        operator,
+        nominal,
+      })
+      .end((_, res) => {
+        expect(res.body.data.phone).to.equal(phone);
+        expect(res.body.data.name).to.equal(name);
+        expect(res.body.data.operator).to.equal(operator);
+        expect(res.body.data.nominal).to.equal(nominal);
+      });
+    done();
+  });
 });
